cc9bd5ce16454164499bc1d282585148ba380875
0xAy4sh <yasheela435@gmail.com>
2025-03-09 15:31:48 +0530

    Update ProjectCard.tsx

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,5 +1,4 @@
 import { useTheme } from '@/context/ThemeContext'
-import Link from 'next/link'
 import React from 'react'
 import { LuShipWheel } from 'react-icons/lu'
 
@@ -8,23 +7,19 @@ const ProjectCard: React.FC = () => {
     return (
         <>
             <div className="flex relative">
-                <Link href="/projects">
+                <a href="/projects" onClick={(e) => {
+                    e.preventDefault();
+                    window.location.href = '/projects';
+                }}>
                     <div className="w-[15rem] border border-zinc-700/45 h-[18rem] rounded-xl project cursor-pointer p-3">
                         <div className="animate-spin-slow inline-block">
                             <LuShipWheel size={65} color="white" />
                         </div>
-                        <h1 className="text font-bold text-5xl tracking-tighter italic pt-2">
-                            PROJECT
-                            <br />
-                            SSS
+                        <h1 className="text font-bold text-4xl tracking-tighter italic pt-2">
+                            PROJECTssss
                         </h1>
                     </div>
-                </Link>
-                <p className={`absolute rtext rotate-[-270deg] bottom-[6.5rem] right-[-4.5rem] lg:right-[-9rem] text-right secf text-md select-none ${theme=='dark'?"text-white":"text-zinc-800"}`}>
-                    {' '}
-                    &quot; the faceless <br />
-                    girl haunting my dreams &quot;{' '}
-                </p>
+                </a>
             </div>
         </>
     )
