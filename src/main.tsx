e3946195da48e104197b90f7741961a59bc98f05
0xAy4sh <yasheela435@gmail.com>
2025-03-10 16:50:29 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/page'
+import Projects from './app/projects/page'
 import './app/globals.css'
 import { ThemeProvider } from './context/ThemeContext'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
