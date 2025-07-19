5cda2cb9d2f5dcdf03f0a621ca7530c03788920c
0xAy4sh <yasheela435@gmail.com>
2025-07-19 19:03:54 +0530

    Fix Spotify song images with working URLs

--- src/constant/songs.ts
+++ src/constant/songs.ts
@@ -3,105 +3,105 @@ export const spotifySongs = [
         name: "Your Name",
         artist: "RADWIMPS",
         spotifyUrl: "https://open.spotify.com/track/0lYBSQXN6rCTvUZvg9S4YU",
-        image: "https://i.scdn.co/image/ab67616d0000b273d3c7b267c2d8b2e7e6b6f1a0",
+        image: "https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Anime"
     },
     {
         name: "Sparkle",
         artist: "RADWIMPS", 
         spotifyUrl: "https://open.spotify.com/track/3A4FRzgve9BjfKbvVXRIFO",
-        image: "https://i.pinimg.com/736x/a2/5b/92/a25b92d843a51f9cb61ae6987f6ff011.jpg",
+        image: "https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Anime"
     },
     {
         name: "Zen Zen Zense",
         artist: "RADWIMPS",
         spotifyUrl: "https://open.spotify.com/track/2aKXeNYKNMS2vZhKDgq3Qs",
-        image: "https://i.pinimg.com/736x/8f/2c/1a/8f2c1a9b4d5e6f7a8b9c0d1e2f3a4b5c.jpg",
+        image: "https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Anime"
     },
     {
         name: "Unravel",
         artist: "TK from Ling tosite sigure",
         spotifyUrl: "https://open.spotify.com/track/4XqJlpYQYhHaVKjUgRgGpF",
-        image: "https://i.pinimg.com/736x/1a/2b/3c/1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d.jpg",
+        image: "https://images.pexels.com/photos/1694900/pexels-photo-1694900.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Anime"
     },
     {
         name: "Cruel Angel's Thesis",
         artist: "Yoko Takahashi",
         spotifyUrl: "https://open.spotify.com/track/3fqRdqzWhiCVdKaLKMZvZw",
-        image: "https://i.pinimg.com/736x/2b/3c/4d/2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e.jpg",
+        image: "https://images.pexels.com/photos/1666065/pexels-photo-1666065.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Anime"
     },
     {
         name: "Blinding Lights",
         artist: "The Weeknd",
         spotifyUrl: "https://open.spotify.com/track/0VjIjW4GlULA8N0NQQkGz",
-        image: "https://i.pinimg.com/736x/3c/4d/5e/3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f.jpg",
+        image: "https://images.pexels.com/photos/1540406/pexels-photo-1540406.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Hollywood"
     },
     {
         name: "Shape of You",
         artist: "Ed Sheeran",
         spotifyUrl: "https://open.spotify.com/track/7qiZfU4dY1lWllzX7mPBI",
-        image: "https://i.pinimg.com/736x/4d/5e/6f/4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a.jpg",
+        image: "https://images.pexels.com/photos/1699161/pexels-photo-1699161.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Hollywood"
     },
     {
         name: "Stay",
         artist: "The Kid LAROI & Justin Bieber",
         spotifyUrl: "https://open.spotify.com/track/5HCyWlXZPP0y6Gqq8TgA20",
-        image: "https://i.pinimg.com/736x/5e/6f/7a/5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b.jpg",
+        image: "https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Hollywood"
     },
     {
         name: "As It Was",
         artist: "Harry Styles",
         spotifyUrl: "https://open.spotify.com/track/4Dvkj6JhhA12EX05fT7y2e",
-        image: "https://i.pinimg.com/736x/6f/7a/8b/6f7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c.jpg",
+        image: "https://images.pexels.com/photos/1190297/pexels-photo-1190297.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Hollywood"
     },
     {
         name: "Anti-Hero",
         artist: "Taylor Swift",
         spotifyUrl: "https://open.spotify.com/track/0V3wPSX9ygBnCm8psDIegu",
-        image: "https://i.pinimg.com/736x/7a/8b/9c/7a8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d.jpg",
+        image: "https://images.pexels.com/photos/1763075/pexels-photo-1763075.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Hollywood"
     },
     {
         name: "Pretender",
         artist: "Official HIGE DANdism",
         spotifyUrl: "https://open.spotify.com/track/2DGa7iaidT5s0qnINlwMjJ",
-        image: "https://i.pinimg.com/736x/8b/9c/0d/8b9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e.jpg",
+        image: "https://images.pexels.com/photos/1694900/pexels-photo-1694900.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Japanese"
     },
     {
         name: "Lemon",
         artist: "Kenshi Yonezu",
         spotifyUrl: "https://open.spotify.com/track/14aTJyOGFdmKJdLLLNFdGF",
-        image: "https://i.pinimg.com/736x/9c/0d/1e/9c0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f.jpg",
+        image: "https://images.pexels.com/photos/1666065/pexels-photo-1666065.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Japanese"
     },
     {
         name: "Kaikai Kitan",
         artist: "Eve",
         spotifyUrl: "https://open.spotify.com/track/1xK1Gg9SxG8fy5ZjNbQnEi",
-        image: "https://i.pinimg.com/736x/0d/1e/2f/0d1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a.jpg",
+        image: "https://images.pexels.com/photos/1540406/pexels-photo-1540406.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Anime"
     },
     {
         name: "Racing Into the Night",
         artist: "YOASOBI",
         spotifyUrl: "https://open.spotify.com/track/6MCjmGYlw6mQVWRFVgBRvB",
-        image: "https://i.pinimg.com/736x/1e/2f/3a/1e2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b.jpg",
+        image: "https://images.pexels.com/photos/1699161/pexels-photo-1699161.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Japanese"
     },
     {
         name: "Demon Slayer",
         artist: "LiSA",
         spotifyUrl: "https://open.spotify.com/track/1QKqpJF8QVy8K5K5K5K5K5",
-        image: "https://i.pinimg.com/736x/2f/3a/4b/2f3a4b5c6d7e8f9a0b1c2d3e4f5a6b7c.jpg",
+        image: "https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=400",
         genre: "Anime"
     }
 ]
